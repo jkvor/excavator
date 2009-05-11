4247c536b4ee420ff5895eec438336874e9bc67d
Jacob Vorreuter <jacobvorreuter@MyDogJesusMac.local>
2009-05-11 08:31:20 -0700

    adding example ex template

--- src/ex_scheduler.erl
+++ src/ex_scheduler.erl
@@ -2,4 +2,11 @@
 -export([add/1]).
 
 %% @spec add()
-add(_) -> ok.
+add(Filename) -> 
+	spawn_link(fun() -> loop(Filename) end).
+	
+loop(Filename) ->
+	io:format("running instructions for ~w~n", [Filename]),
+	Instrs = ex_pp:parse(Filename),
+	ok = ex_engine:run(Instrs),
+	loop(Filename).
