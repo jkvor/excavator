4d3538b2d38bd8ab88a53a5eff23af981133b988
Jacob Vorreuter <jacobvorreuter@MyDogJesusMac.local>
2009-05-10 18:10:24 -0700

    handling more instrs

--- src/ex_pp.erl
+++ src/ex_pp.erl
@@ -49,11 +49,40 @@ build_instrs([], Acc) -> Acc;
 build_instrs([Instr|Tail], Acc) ->
 	build_instrs(Tail, [build_instr(Instr)|Acc]).
 
-build_instr({call, _, {atom, _, Instr}, Args}) ->
+build_instr({call, _, {atom, _, each}, [Key, Source, Instrs]}) ->
+	{tuple, ?L, [{atom,?L,instr}, {atom,?L,each}, to_cons([Key, Source, build_instr(Instrs)])]};
+
+build_instr({call, _, {atom, _, onfail}, [Error, Instrs]}) ->
+	{tuple, ?L, [{atom,?L,instr}, {atom,?L,onfail}, to_cons([Error, build_instr(Instrs)])]};
+	
+build_instr({cons,_,Instr,{nil,_}}) ->
+	{cons, ?L, build_instr(Instr), {nil,?L}};
+	
+build_instr({cons,_,Instr,Cons}) ->
+	{cons, ?L, build_instr(Instr), build_instr(Cons)};
+	
+build_instr({call, _, {atom, _, Instr}, Args}) 
+	when Instr==configure; Instr==assign; Instr==assert; 
+		 Instr==fetch; Instr==print; Instr==function; 
+		 Instr==onfail; Instr==commit ->
 	{tuple, ?L, [{atom,?L,instr}, {atom,?L,Instr}, to_cons(Args)]};
 	
 build_instr(Instr) ->
 	exit({unrecognized_instruction, Instr}).
 	
+preprocess_arg({tuple, _, [{atom,_,regexp},{atom,_,Key},{string,_,Regexp}]}) ->
+	{ok, {re_pattern, A, B, Bin}} = re:compile(Regexp),
+	Pattern = {tuple,?L,[
+				{atom,?L,re_pattern},
+	        	{integer,?L,A},
+	        	{integer,?L,B},
+	        	{bin,?L,
+					[{bin_element,?L,{integer,?L,I},default,default} || I <- binary_to_list(Bin)]
+				}
+			  ]},
+	{tuple, ?L, [{atom,?L,regexp},{atom,?L,Key},Pattern]};
+	
+preprocess_arg(Arg) -> Arg.
+	
 to_cons([]) -> {nil, ?L};
-to_cons([Arg|Tail]) -> {cons,?L,Arg,to_cons(Tail)}.
+to_cons([Arg|Tail]) -> {cons,?L,preprocess_arg(Arg),to_cons(Tail)}.
